985b68a12d845e4dba32bf131de25ff474171804
Venterok <happyhaster@gmail.com>
2022-08-18 08:55:03 +0300

    rewrite

--- main.py
+++ main.py
@@ -1,22 +1,78 @@
-from telethon import TelegramClient, events
-from discord_webhook import DiscordWebhook
+import asyncio
+import os
+import random
+import shutil
 
-api_id = 10294483
-api_hash = 'feef69d0322a4b4bd08f50f2e028903d'
+import pyimgur
+from discord_webhook import DiscordWebhook, DiscordEmbed
+from telethon import TelegramClient, events, utils
 
-my_channel_id = '-1696108040'
-channels = [my_channel_id]
+api_id = 'api_id'   # Client Telegram api_id
+api_hash = 'api_hash'  # Client Telegram api_hash
+
+CLIENT_ID = 'CLIENT_ID'  # Imgur Client ID
+
+channels = {
+    1696108040: [
+        'webhook-link',
+        'telegram-channel-link',
+        'telegram-channel-avatar'],
+    1360155440: [
+        'webhook-link',
+        'telegram-channel-link',
+        'telegram-channel-avatar']
+}
 
 client = TelegramClient('Sunblogs', api_id, api_hash)
-print("TeleDis запущен")
+print("TG2DS Started.")
 
 
 @client.on(events.NewMessage())
 async def my_event_handler(event):
-    webhook = DiscordWebhook(
-        url='https://discord.com/api/webhooks/1008034602360778822/z9CBESdQFb28f_MSR0B1OaT3fpb5l9_dAMMbdh-7z70EruttvxsNzt4zoONvpJyJkWyl',
-        content=event.message.text)
+    # Getting settings from dict
+    channeldata = channels[event.message.peer_id.channel_id]
+    webhook = DiscordWebhook(url=channeldata[0])
+    # Hex color shit
+    hexcolor = "{:06x}".format(random.randint(0, 0xFFFFFF))  # TODO Hex color from image
+
+    # Get Chat Name
+    chat_from = event.chat if event.chat else (await event.get_chat())
+    chat_title = utils.get_display_name(chat_from)
+
+    # Set text to embed
+    embed = DiscordEmbed(description=event.message.text + "\n\n" + channeldata[1], color=hexcolor)
+
+    # If message contain photo
+    if event.photo:
+        saved_path = await event.download_media('temp')
+        await asyncio.sleep(3)
+        PATH = saved_path
+
+        im = pyimgur.Imgur(CLIENT_ID)
+        uploaded_image = im.upload_image(PATH, title="Uploaded with PyImgur")
+        embed.set_image(url=uploaded_image.link)
+
+    # If author & footer & timestamp
+    embed.set_footer(text=chat_title, icon_url=channeldata[2])
+    embed.set_author(name='Новый пост!')
+    embed.set_timestamp()
+
+    # Send this shit
+    webhook.add_embed(embed)
     response = webhook.execute()
+
+    # Clear all files in package temp
+    folder = 'temp'
+    for filename in os.listdir(folder):
+        file_path = os.path.join(folder, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
+
 
 
 client.start()
